8c1898ba8023935797cf6e6cfeeea638454b7eab
reynardaj <reynard.joshua4949@gmail.com>
2024-12-22 08:49:25 +0700

    added simple structure and routing for coping

---
+++ src/pages/Focus.tsx
@@ -0,0 +1,9 @@
+function Focus() {
+  return (
+    <div>
+      <h1>Focus</h1>
+    </div>
+  );
+}
+
+export default Focus;
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Home from '../pages/Home';
 import Breathe from '../pages/Breathe';
+import Coping from '../pages/Coping';
+import Focus from '../pages/Focus';
 
 const AppRouter = () => {
   return (
@@ -8,6 +10,8 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/breathe" element={<Breathe />} />
+        <Route path="/coping" element={<Coping />} />
+        <Route path="/focus" element={<Focus />} />
       </Routes>
     </Router>
   );
